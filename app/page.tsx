7b1272334bd4ea0a0ad3d45e9eedc01a467ba0ba
CrystalCodes01 <crystalcodes.0@gmail.com>
2025-06-23 14:24:47 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+import React from "react";
+
 export default function Home() {
+  // This will break — Next 15 doesn’t support React 19's experimental React.use
+  const data = React.use(async () => await Promise.resolve("hi"));
+
   return (
     <div className="appContainer">
       <div className="container">
         <h1>o2x - Home</h1>
+        <div>{data}</div>
       </div>
     </div>
   );
